bb5789ea61b4430f9c856cc09f640eec3f4e65de
IlyessDev <94439679+IlyessDev@users.noreply.github.com>
2026-03-07 12:11:55 +0100

    Update api.js

--- js/api.js
+++ js/api.js
@@ -32,7 +32,7 @@ export function insertVideo(video) {
   return fetch(SUPABASE_URL +'/videos', {
     method: 'POST',
     headers: {
-      getHeaders(),
+      ...getHeaders(),
       'Content-Type': 'application/json',
       'Prefer': 'return=representation'
     },
@@ -48,7 +48,7 @@ export function updateVideo(id, updates) {
   return fetch(SUPABASE_URL + '/videos?id=eq.' + id, {
     method: 'PATCH',
     headers: {
-       getHeaders(),
+       ...getHeaders(),
       'Content-Type': 'application/json',
       'Prefer': 'return=representation'
     },
