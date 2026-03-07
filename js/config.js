c0c3f0dfe6cfe6699133d4f7c6dbcc42526ac535
IlyessDev <94439679+IlyessDev@users.noreply.github.com>
2026-03-07 12:00:49 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,14 @@
-// URL de base de l'API Supabase
+// URL base de donnée
 export const SUPABASE_URL = 'https://rrqhgcebyyiagfmuvwbq.supabase.co/rest/v1';
 
-// Clés d'authentification
-export const SUPABASE_HEADERS = {
-  'apikey': 'sb_publishable_SVg5J0A-rQMRIMSGO8brqg_lOehnsCq',
-  'Authorization': 'Bearer sb_publishable_SVg5J0A-rQMRIMSGO8brqg_lOehnsCq'
-};
+// clé public bdd
+export const API_KEY = 'sb_publishable_SVg5J0A-rQMRIMSGO8brqg_lOehnsCq';
+
+//récuperer token
+export function getHeaders() {
+  const token = localStorage.getItem('token') || API_KEY;
+  return {
+    'apikey': API_KEY,
+    'Authorization': 'Bearer ' + token
+  };
+}
