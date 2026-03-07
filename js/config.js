75b318b7101a93dbe34f59604c1fd2663c385def
IlyessDev <94439679+IlyessDev@users.noreply.github.com>
2026-03-07 13:55:43 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -9,6 +9,6 @@ export function getHeaders() {
   const token = localStorage.getItem('token') || API_KEY;
   return {
     'apikey': API_KEY,
-    'Authorization': 'Bearer ' + token
+    'Authorization': 'Bearer ' + (token || API_KEY)
   };
 }
